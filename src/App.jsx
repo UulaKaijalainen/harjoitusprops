b155697a3bb16200364110f204d87d9778daf534
Uula <uula.art@gmail.com>
2025-01-22 12:41:56 +0200

    palautus

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hello from './Hello.jsx'
+
+
+function App() {
+
+  return (
+    <div>
+      <Hello name="Ville" />
+    </div>
+  )
+};
+
+export default App
---
+++ src/Hello.jsx
@@ -0,0 +1,7 @@
+function Hello({name}){
+    console.log(name);
+    return(
+        <p>hei  {name}! </p>
+    )
+}
+export default Hello
